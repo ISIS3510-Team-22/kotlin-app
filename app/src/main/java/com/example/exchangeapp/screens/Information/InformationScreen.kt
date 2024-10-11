ef0ab91f64f59e4cd720024b5c561f85202e7f41
Carlos Garcia <ca.garciag1@uniandes.edu.co>
2024-10-10 20:44:31 -0500

    Menu button added Calendar Icon added 5 button grid added

--- app/src/main/java/com/example/exchangeapp/screens/Information/InformationScreen.kt
+++ app/src/main/java/com/example/exchangeapp/screens/Information/InformationScreen.kt
@@ -1,22 +1,35 @@
 package com.example.exchangeapp.screens.Information
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -26,33 +39,72 @@ fun InformationScreen(
     modifier: Modifier = Modifier,
     viewModel: InformationViewModel = hiltViewModel()
 ){
+    val buttontexts = listOf("Cooking & recipes while abroad", "Mental Health", "Adapting to a new city", "Universities info", "Current exchanges available")
+
     Column (
         modifier = modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .verticalScroll(rememberScrollState())
-            .background(Color("#0F3048".toColorInt())),
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .fillMaxSize()
+            .background(Color("#0F3048".toColorInt()))
     ){
-        Row(verticalAlignment = Alignment.Top) {
-            //Image( Icons.Rounded.Menu)
-            Text("Information", style = MaterialTheme.typography.headlineMedium, modifier = modifier.padding(vertical = 10.dp))
+        Row(
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.Start
+        ){
+            IconButton(
+                onClick = {viewModel.onMenuClick(openAndPopUp)},
+                modifier =modifier
+                    .size(100.dp)
+                    .padding(25.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Menu, // Using the built-in menu icon
+                    contentDescription = "",
+                    modifier = modifier
+                        .size(60.dp)
+                )
+            }
+            Spacer(modifier = modifier.width(60.dp))
+
+            Text("INFORMATION", modifier = modifier.padding(vertical = 15.dp).align(Alignment.CenterVertically), style = MaterialTheme.typography.headlineLarge )
+
+            Spacer(modifier = modifier.width(65.dp))
+
+            Icon(
+                imageVector = Icons.Filled.CalendarToday,
+                contentDescription = "",
+                modifier = modifier
+                    .size(80.dp)
+                    .padding(vertical = 15.dp)
+            )
         }
-        Button( onClick = {viewModel.onChatClick(openAndPopUp)},
-            shape = CutCornerShape(5),
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(horizontal = 20.dp, vertical = 5.dp)
-        ) {
-            Text("Cooking & recipies while abroad")
+
+        Spacer(Modifier.height(20.dp))
+
+        LazyColumn {
+            items(buttontexts){ label ->
+                LabeledButtons(label, viewModel.onChatClick(openAndPopUp), modifier)
         }
-        Button( onClick = {viewModel.onChatClick(openAndPopUp)},
-            shape = CutCornerShape(5),
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(horizontal = 20.dp, vertical = 5.dp)
-        ) {
-            Text("Mental Health")
+        }
         }
     }
+
+
+@Composable
+fun LabeledButtons(text : String, onClick : Unit, modifier: Modifier){
+        Button(
+            onClick = {onClick},
+            shape = RoundedCornerShape(35),
+            modifier = modifier
+                .padding(horizontal = 20.dp, vertical = 15.dp)
+                .fillMaxWidth()
+                .height(120.dp),
+            colors = ButtonColors(
+                Color("#18354d".toColorInt()),
+                Color.White,
+                MaterialTheme.colorScheme.tertiary,
+                MaterialTheme.colorScheme.onTertiary
+            )
+        ) {
+            Text(text = text, fontSize = 20.sp, style = MaterialTheme.typography.bodyMedium, fontWeight = FontWeight.Bold)
+        }
 }
--- app/src/main/java/com/example/exchangeapp/screens/Information/InformationViewModel.kt
+++ app/src/main/java/com/example/exchangeapp/screens/Information/InformationViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.exchangeapp.screens.Information
 
 import com.example.exchangeapp.CHAT_SCREEN
+import com.example.exchangeapp.INFO_SCREEN
+import com.example.exchangeapp.MENU_SCREEN
 import com.example.exchangeapp.NAVIGATION_SCREEN
 import com.example.exchangeapp.screens.ExchangeAppViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,6 +14,12 @@ class InformationViewModel @Inject constructor(): ExchangeAppViewModel() {
     fun onChatClick(openAndPopUp : (String,String) -> Unit){
         launchCatching {
             openAndPopUp(CHAT_SCREEN, NAVIGATION_SCREEN)
+        }
+    }
+
+    fun onMenuClick(openAndPopUp : (String,String) -> Unit){
+        launchCatching {
+            openAndPopUp(MENU_SCREEN, INFO_SCREEN)
         }
     }
 }
