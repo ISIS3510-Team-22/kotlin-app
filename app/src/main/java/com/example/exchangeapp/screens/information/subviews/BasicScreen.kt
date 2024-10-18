2e62f1d113c2cebe4fcb87c281b2e33f99894045
Carlos Garcia <ca.garciag1@uniandes.edu.co>
2024-10-17 19:48:15 -0500

    Small UI fix (scroll)

--- app/src/main/java/com/example/exchangeapp/screens/information/subviews/BasicScreen.kt
+++ app/src/main/java/com/example/exchangeapp/screens/information/subviews/BasicScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.filled.ExpandMore
@@ -57,6 +59,7 @@ fun BasicScreen(
             .fillMaxSize()
             .background(Color(0xFF0F3048))
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Row {
             IconButton(
