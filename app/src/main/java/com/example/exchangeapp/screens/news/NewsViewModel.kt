88cb9e23958a7bd67bc5b1115f83bd5901f466f2
Carlos Garcia <ca.garciag1@uniandes.edu.co>
2024-11-04 16:35:26 -0500

    Analytics
    Added logic for logging to the pipeline

--- app/src/main/java/com/example/exchangeapp/screens/news/NewsViewModel.kt
+++ app/src/main/java/com/example/exchangeapp/screens/news/NewsViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.exchangeapp.MENU_SCREEN
 import com.example.exchangeapp.screens.ExchangeAppViewModel
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -14,9 +16,10 @@ class NewsViewModel @Inject constructor() : ExchangeAppViewModel() {
 
     init {
         _webPageUrls.value = listOf(
-            "https://internacionalizacion.uniandes.edu.co/programas-movilidad-academica/intercambio-semestral",
-            "https://sistemas.uniandes.edu.co/es/isis-oportunidades/informacion-general-oportunidades#tab-intercambios-en-el-exterior",
-            "https://mecanica.uniandes.edu.co/es/estudiantes/pregrado/intercambio-internacional"
+            "https://www.theguardian.com/education",
+            "https://www.nytimes.com/international/section/education",
+            "https://www.euronews.com/tag/entertainment",
+            "https://www.ft.com/eu-economy"
         )
     }
 
@@ -24,6 +27,16 @@ class NewsViewModel @Inject constructor() : ExchangeAppViewModel() {
         launchCatching {
             open(MENU_SCREEN)
         }
+    }
+
+    fun onWebViewClick(url:String){
+        val categories = mapOf(
+            "https://www.theguardian.com/education" to "Education",
+            "https://www.nytimes.com/international/section/education" to "Education",
+            "https://www.euronews.com/tag/entertainment" to "Entertainment",
+            "https://www.ft.com/eu-economy" to "Economy"
+        )
+        categories[url]?.let { Firebase.analytics.logEvent(it, null) }
     }
 
 }
